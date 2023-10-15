321446b22da9482344aa7c6190071b025cd814b2
Паластрова Станислава <palastrovastasa@gmail.com>
2023-10-15 17:20:41 +0500

    Больше деталей

---
+++ js/main.js
@@ -0,0 +1,82 @@
+const PICTURE_COUNT = 25;
+const AVATAR_COUNT = 6;
+const LIKE_MIN_COUNT = 15;
+const LIKE_MAX_COUNT = 200;
+const COMMENT_COUNT = 30;
+const COMMENT_LINES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+]
+const DESCRIPTION = [
+  'Мне кажется, что я выгляжу просто шикарно. Надеюсь, что это кажется не только мне',
+  'Все мои волки делают АуФ',
+  'Ещё один день. Ещё одно фото.',
+  'Самое любимое время года у меня - лето, оно таинственно прекрасно #лето #красота #отдых #развлечения',
+  'Сериальчики, вкусняшки, плед и чай - что ещё может быть лучше осенью #',
+  'Настроение - пятница',
+  'Нет, я не в Дубае #я #не #отдыхаю',
+  'Не стыдись того, кто ты есть. Меняйся для себя в лучшую сторону #мотивация #любовькжизни #доброеутро',
+  'Ну фотка',
+  'Как мало нужно для счастья... Жду комменты'
+]
+const NAMES = [
+  'Евдоким', 'Авдотья', 'Кирилл', 'Августин', 'Елизавета', 'Стейси', 'Вася', 'Гена', 'Лена', 'Лев'
+]
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const generateID = () => {
+  let lastGeneratedID = 0;
+  return () => {
+    lastGeneratedID += 1;
+    return lastGeneratedID;
+  };
+};
+
+const generateCommentID = generateID();
+
+const createMessage = () => Array.from(
+  { length: getRandomInteger(1, 2) },
+  () => getRandomArrayElement(COMMENT_LINES),
+).join(' ');
+
+const createComment = () => {
+  return {
+    id: generateCommentID(),
+    avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
+    message: createMessage(),
+    name: getRandomArrayElement(NAMES),
+  }
+};
+
+const createPicture = (index) => {
+  return {
+    id: index,
+    url: `photos/${index}.jpg`,
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+    comments: Array.from(
+      { length: getRandomInteger(0, COMMENT_COUNT) },
+      createComment,
+    ),
+  }
+};
+
+const getPictures = () => Array.from (
+  { length : PICTURE_COUNT },
+  (_, pictureIndex) => createPicture(pictureIndex + 1),
+);
+getPictures();
+
+
