a3ab2618200f2864c35bf5e4b76d69ded1e13358
Паластрова Станислава <palastrovastasa@gmail.com>
2023-10-22 22:44:57 +0500

    Функции возвращаются

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+function countTimeInMinutes(time) {
+  const [hour, minute] = time.split(':');
+  return Number(hour) * 60 + Number(minute);
+}
+
+function checkTime(dayStart, dayEnd, meetingStart, meetingDuration) {
+  const dayStartInMinutes = countTimeInMinutes(dayStart);
+  const dayEndInMinutes = countTimeInMinutes(dayEnd);
+  const meetingStartInMinutes = countTimeInMinutes(meetingStart);
+
+  return (dayStartInMinutes <= meetingStartInMinutes && meetingStartInMinutes + meetingDuration <= dayEndInMinutes);
+}
+
