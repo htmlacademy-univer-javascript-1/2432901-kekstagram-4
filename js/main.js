5b2af01e55ea6514dbbcd310ab186dbdd698c384
Паластрова Станислава <palastrovastasa@gmail.com>
2023-11-18 20:40:58 +0500

    Отрисуй меня полностью

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { getPictures } from './data.js';
+import { renderMiniatures } from './miniatures.js';
+
+renderMiniatures(getPictures());
+
---
+++ js/miniatures.js
@@ -0,0 +1,21 @@
+const miniatureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+
+const createMiniature = ({ comments, description, likes, url }) => {
+  const miniature = miniatureTemplate.cloneNode(true);
+  miniature.querySelector('.picture__comments').textContent = comments;
+  miniature.querySelector('.picture__img').alt = description;
+  miniature.querySelector('.picture__likes').textContent = likes;
+  miniature.querySelector('.picture__img').src = url;
+  return miniature;
+};
+
+const renderMiniatures = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    fragment.append(createMiniature(picture));
+  });
+  container.append(fragment);
+};
+
+export { renderMiniatures };
