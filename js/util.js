38fcdbc441dca4dcdae7d8a5aa7f375e98b745be
Паластрова Станислава <palastrovastasa@gmail.com>
2023-12-20 18:59:15 +0500

    добавлена функция throttle

--- js/util.js
+++ js/util.js
@@ -43,4 +43,15 @@ const debounce = (callback, timeoutDelay = 500) => {
   };
 };
 
-export { getRandomArrayElement, isEscapeKey, getRandomInteger, showAlert, debounce };
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export { getRandomArrayElement, isEscapeKey, getRandomInteger, showAlert, debounce, throttle };
